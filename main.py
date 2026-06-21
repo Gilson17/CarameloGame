0260d3d8907bae33881b54f559ead5fbd5080131
Gilson17 <gilson.work@hotmail.com>
2026-06-21 11:36:22 -0300

    adding classes from star UML

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ while True:
     # check for all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            pygame.quit() #close window
             quit()
